4cf154e7ce7bfa86620aefad9dd4168f4952af6a
Antía <antiagarrido2.0@gmail.com>
2023-11-09 23:12:50 +0100

    New contact

--- src/app/contact-new/contact-new.component.ts
+++ src/app/contact-new/contact-new.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { ContactsService } from "../contacts.service";
 
 @Component({
-  selector: 'app-contact-new',
-  templateUrl: './contact-new.component.html',
-  styleUrls: ['./contact-new.component.css']
+  selector: "app-contact-new",
+  templateUrl: "./contact-new.component.html",
+  styleUrls: ["./contact-new.component.css"],
 })
 export class ContactNewComponent implements OnInit {
+  name: string;
+  surname: string;
+  second_surname: string;
+  phone: string;
+  email: string;
 
-  constructor() { }
+  constructor(private router: Router, private contactService: ContactsService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  newContact() {
+    const contact = {
+      name: this.name,
+      surname: this.surname,
+      second_surname: this.second_surname,
+      phone: this.phone,
+      email: this.email,
+    }
+    this.contactService.newContact(contact);
+    this.navigateToHome();
   }
 
-}
+    cancelInsert(){
+      this.navigateToHome();}
+
+      navigateToHome(){
+        this.router.navigate(['/contacts']);
+      }
+
+  }
