fd5db79a5cab310cda66a40cfe39d9b72c3be02c
Antía <antiagarrido2.0@gmail.com>
2023-11-14 23:33:18 +0100

    ahora sí que works

--- src/app/contact-edit/contact-edit.component.ts
+++ src/app/contact-edit/contact-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactsService } from '../contacts.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-edit',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-edit.component.css']
 })
 export class ContactEditComponent implements OnInit {
+  contact: any;
 
-  constructor() { }
+  constructor(private contactService: ContactsService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.contactService.getContact(this.route.snapshot.params['id']).subscribe(data =>{
+      this.contact = data;
+    })
   }
 
+  updateContact() {
+    this.contactService.updateContact(this.contact);
+    this.navigateContactDetail();
+  }
+
+  cancelUpdate() {
+    this.navigateContactDetail();
+  }
+  
+  navigateContactDetail() {
+    this.router.navigate(['/contact', this.route.snapshot.params['id']])
+  }
 }
