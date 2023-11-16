de54211fcae1af24c1e2e7a7696f9fce21c0f25e
Antía <antiagarrido2.0@gmail.com>
2023-11-17 00:05:25 +0100

    charts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,10 +8,12 @@ import { ContactNewComponent } from './contact-new/contact-new.component';
 import { ProductNewComponent } from './product-new/product-new.component';
 import { ContactEditComponent } from './contact-edit/contact-edit.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ChartsComponent } from './charts/charts.component';
 
 
 
 const routes: Routes = [
+  {path: '', component: ChartsComponent},
   {path: 'contacts', component: ContactHomeComponent},
   {path: 'contact/new', component: ContactNewComponent},
   {path: 'contact/edit/:id', component: ContactEditComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,45 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ContactHomeComponent } from "./contact-home/contact-home.component";
-import { MatButtonModule, MatCardModule, MatDialogModule, MatIconModule, MatInputModule, MatSelectModule, MatTableModule, MatToolbarModule } from "@angular/material";
-import {HttpClientModule} from '@angular/common/http';
-import { ContactDetailComponent } from './contact-detail/contact-detail.component';
-import { ProductHomeComponent } from './product-home/product-home.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ContactNewComponent } from './contact-new/contact-new.component';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDialogModule,
+  MatIconModule,
+  MatInputModule,
+  MatSelectModule,
+  MatTableModule,
+  MatToolbarModule,
+} from "@angular/material";
+import { HttpClientModule } from "@angular/common/http";
+import { ContactDetailComponent } from "./contact-detail/contact-detail.component";
+import { ProductHomeComponent } from "./product-home/product-home.component";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
+import { ContactNewComponent } from "./contact-new/contact-new.component";
 import { FormsModule } from "@angular/forms";
-import { ProductNewComponent } from './product-new/product-new.component';
-import { ContactEditComponent } from './contact-edit/contact-edit.component';
-import { ProductEditComponent } from './product-edit/product-edit.component';
-import { ContactDeleteComponent } from './contact-delete/contact-delete.component';
-import { ProductDeleteComponent } from './product-delete/product-delete.component';
-
-
+import { ProductNewComponent } from "./product-new/product-new.component";
+import { ContactEditComponent } from "./contact-edit/contact-edit.component";
+import { ProductEditComponent } from "./product-edit/product-edit.component";
+import { ContactDeleteComponent } from "./contact-delete/contact-delete.component";
+import { ProductDeleteComponent } from "./product-delete/product-delete.component";
+import { ChartsComponent } from "./charts/charts.component";
+import { NgxChartsModule } from "@swimlane/ngx-charts";
 
 @NgModule({
-  declarations: [AppComponent, ContactHomeComponent, ContactDetailComponent, ProductHomeComponent, ProductDetailComponent, ContactNewComponent, ProductNewComponent, ContactEditComponent, ProductEditComponent, ContactDeleteComponent, ProductDeleteComponent],
+  declarations: [
+    AppComponent,
+    ContactHomeComponent,
+    ContactDetailComponent,
+    ProductHomeComponent,
+    ProductDetailComponent,
+    ContactNewComponent,
+    ProductNewComponent,
+    ContactEditComponent,
+    ProductEditComponent,
+    ContactDeleteComponent,
+    ProductDeleteComponent,
+    ChartsComponent,
+  ],
   entryComponents: [ContactDeleteComponent, ProductDeleteComponent],
   imports: [
     BrowserModule,
@@ -34,8 +56,10 @@ import { ProductDeleteComponent } from './product-delete/product-delete.componen
     MatIconModule,
     FormsModule,
     MatInputModule,
-    MatSelectModule, 
+    MatSelectModule,
     MatDialogModule,
+    NgxChartsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent],
