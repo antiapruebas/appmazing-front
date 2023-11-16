b1d7d0b5fd12c83805c6359d0e65d009d572666e
Antía <antiagarrido2.0@gmail.com>
2023-11-16 13:28:25 +0100

    product

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../products.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
@@ -12,12 +12,22 @@ export class ProductDetailComponent implements OnInit {
   product: any;
 
   constructor( private productsService: ProductsService, 
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private router: Router,) { }
 
   ngOnInit() {
     this.productsService.getProduct(this.route.snapshot.params["id"]).subscribe((data)=> {
       this.product = data;
     })
   }
+
+  editProduct(){
+    this.router.navigate(['/product/edit', this.route.snapshot.params['id']]);
+  }
+
+  closeProduct(){
+    this.router.navigate(['/products']);
+  }
+
 
 }
