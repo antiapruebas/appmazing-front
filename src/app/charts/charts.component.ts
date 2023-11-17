61d4dce4599d632e79e4173dc64419c3e0664cd4
Antía <antiagarrido2.0@gmail.com>
2023-11-17 11:31:51 +0100

    all charts contacts

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -9,56 +9,113 @@ import { ContactsService } from "../contacts.service";
 export class ChartsComponent implements OnInit {
   initialLetter = [];
   contactsByFullName = [];
+  emailExtensions = [];
+  phonePrefixData = [];
 
   constructor(private contactService: ContactsService) {}
 
   ngOnInit() {
     this.contactService.getContacts().subscribe((data) => {
       this.initialLetter = this.calculateInitialLettersData(data);
-    this.contactsByFullName= this.calculateContactsByFullNameData(data);
-  })
-}
+      this.contactsByFullName = this.calculateContactsByFullNameData(data);
+      this.emailExtensions = this.calculateEmailExtensionsData(data);
+      this.phonePrefixData = this.generatePhonePrefixData(data);
+    });
+  }
 
   calculateInitialLettersData(contacts: any[]): any {
-    return contacts.reduce((result,contact) => {
+    return contacts.reduce((result, contact) => {
       const initial = contact.surname.charAt(0).toUpperCase();
-      if (result.find(item =>item.name === initial)) {
-        result.find(item => item.name === initial).value++
-      }else{
-        result.push({name:initial, value: 1})
+      if (result.find((item) => item.name === initial)) {
+        result.find((item) => item.name === initial).value++;
+      } else {
+        result.push({ name: initial, value: 1 });
       }
       return result;
-      }, []);
-
+    }, []);
   }
 
-  calculateContactsByFullNameData(contacts: any[]): any{
-    let tempContactsByFullName =[{
-    name: 'Contacts',
-    series: []
+  calculateContactsByFullNameData(contacts: any[]): any {
+    let tempContactsByFullName = [
+      {
+        name: "Contacts",
+        series: [],
+      },
+    ];
+    contacts.forEach((contact) => {
+      let fullName = contact.name + contact.surname;
+      if (contact.second_surname) {
+        fullName = fullName + contact.second_surname;
+      }
+      const size = fullName.length;
+      const range = `${size - (size % 5)} - ${size - (size % 5) + 4} ch .`;
+      let existingRange = tempContactsByFullName[0].series.find(
+        (item) => item.name === range
+      );
+      if (existingRange) {
+        existingRange.value++;
+      } else {
+        tempContactsByFullName[0].series.push({ name: range, value: 1 });
+      }
+    });
 
-  }];
-  contacts.forEach(contact => {
-    let fullName = contact.name + contact. surname;
-    if(contact.second_surname) {
-    fullName = fullName + contact.second_surname 
-    }
-    const size = fullName.length;
-    const range = `${size - (size % 5)} - ${size - (size % 5) +4} ch .`;
-    let existingRange = tempContactsByFullName[0].series.find(item => item.name === range)
-  if (existingRange) {
-    existingRange.value++;
-  } else{
-    tempContactsByFullName[0].series.push({name:range, value:1})
+    return tempContactsByFullName.map((entry) => {
+      return {
+        ...entry,
+        series: entry.series.sort(
+          (a, b) => Number(a.name.split("-")[0]) - Number(b.name.split("-")[0])
+        ),
+      };
+    });
   }
-  });
 
-  return tempContactsByFullName.map(entry =>{
-    return{
-      ...entry,
-      series: entry.series.sort((a,b)=> Number (a.name.split('-')[0])- Number (b.name.split('-')[0]))
-    }
-    })
+  calculateEmailExtensionsData(contacts: any[]): any {
+    let emailExtensionsMap = new Map<string, number>();
+    contacts.forEach((contact) => {
+      let emailParts = contact.email.split("@");
+      if (emailParts.length == 2) {
+        const domain = emailParts[1];
+        const firstDot = domain.indexOf(".");
+        if (firstDot != -1) {
+          const extensionFromDot = domain.substring(firstDot);
+          if (emailExtensionsMap.has(extensionFromDot)) {
+            emailExtensionsMap.set(
+              extensionFromDot,
+              emailExtensionsMap.get(extensionFromDot) + 1
+            );
+          } else {
+            emailExtensionsMap.set(extensionFromDot, 1);
+          }
+        }
+      }
+    });
+    let emailExtensions = [];
+    emailExtensionsMap.forEach((value, key) => {
+      emailExtensions.push({ name: key, value: value });
+    });
+    return emailExtensions;
+  }
+
+  //------------------------------------------------------------------------------------------------
+
+  generatePhonePrefixData(contacts: any[]): any {
+    let phonePrefixData = [];
+    let prefixCounts = {};
+    contacts.forEach((contact) => {
+      const phonePrefix = contact.phone.substring(0, 1);
+      if (prefixCounts[phonePrefix]) {
+        prefixCounts[phonePrefix]++;
+      } else {
+        prefixCounts[phonePrefix] = 1;
+      }
+    });
+
+    for (let prefix in prefixCounts) {
+      if (prefixCounts.hasOwnProperty(prefix)) {
+        phonePrefixData.push({ name: prefix, value: prefixCounts[prefix] });
+      }
     }
 
+    return phonePrefixData;
+  }
 }
